7ad74736085128959dcec882e6237ec8e2b1798b
jinalshah999 <jinalshah999@gmail.com>
2019-12-11 19:44:55 +0530

    added custom validations

--- secondapp/src/app/user-signup/user-signup.component.ts
+++ secondapp/src/app/user-signup/user-signup.component.ts
@@ -1,23 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  AbstractControl
+} from "@angular/forms";
 
 @Component({
-  selector: 'app-user-signup',
-  templateUrl: './user-signup.component.html',
-  styleUrls: ['./user-signup.component.css']
+  selector: "app-user-signup",
+  templateUrl: "./user-signup.component.html",
+  styleUrls: ["./user-signup.component.css"]
 })
 export class UserSignupComponent implements OnInit {
-user_signup:FormGroup;
-  constructor() { }
-
+  user_signup: FormGroup;
+  constructor() {}
+  invalidNames: string[] = ["jinal", "shah"];
   ngOnInit() {
-    this.user_signup=new FormGroup({
-      user_name:new FormControl(null,[Validators.required,Validators.minLength(5),Validators.pattern('[a-zA-z]*')]),
-      user_password:new FormControl(null)
+    this.user_signup = new FormGroup({
+      user_name: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.pattern("[a-zA-z]*")
+      ]),
+      password_group: new FormGroup(
+        {
+          user_password: new FormControl(null, Validators.required),
+          user_confirm_password: new FormControl(null)
+        },
+        this.matchPassword.bind(this)
+      )
     });
   }
-  onUserAdd(){
+  onUserAdd() {
     console.log(this.user_signup);
   }
 
+  invalidName(c: AbstractControl): { [key: string]: boolean } {
+    if (this.invalidNames.indexOf(c.value) >= 0) {
+      return { customError: true };
+    }
+    return null;
+  }
+  matchPassword(c: AbstractControl): { [key: string]: boolean } {
+    if (c.get("user_password").value !== c.get("user_confirm_password").value) {
+      return { sarkhanathi: true };
+    }
+    return null;
+  }
 }
